27bcd720174c7759517150a60cb0af00c99778d4
Anjum269 <01fe24bca044.kletech@gmail.com>
2025-11-04 11:50:06 +0530

    Fix variable assignment and improve output formatting

---
+++ std.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.arg)==3:
+script_name =sys.argv[0]
+name = sys.argv[1]
+rollno =sys.argv[2]
+print("User provided input values:")
+else:
+script name = sys.argv[0]
+name = "Anjum
+rollno = "044"
+print ("No input given - using default values:")
+print("script Name:",script_name)
+print("student Name:",name)
+print("Roll Number:",rollno)
